d2e1bfb19c8acd67070a0e623eabf827f8e74b1c
Pouyan Zarbafian <pouyan.zarbafian@hotmail.com>
2018-12-05 10:46:31 -0500

    starting server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
+const parseXlsx =  require('parseXlsx');
+
 const express = require('express'),
     app = express();
 const PORT = process.env.PORT || 8080;
+
+app.parseXlsx('Info-1.xlsx').then((data) => {
+    // data is an array of arrays
+    console.log(data);
+  });
+  
+
 
 app.listen(PORT, ()=>{
     console.log(`server running on ${PORT}`);
