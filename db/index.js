f32032cc2a68f763dc9718a57c8107d1f72e3f4a
Pouyan <pouyan.zarbafian@hotmail.com>
2024-01-30 00:17:35 -0500

    fix routes

--- db/index.js
+++ db/index.js
@@ -1,114 +1,139 @@
-const express = require('express'),
-    app = express(),
-    Sequelize = require('sequelize'),
-    cors = require('cors'),
-    Op = Sequelize.Op;
+var XLSX = require("xlsx");
+const express = require("express"),
+  app = express(),
+  // Sequelize = require('sequelize'),
+  cors = require("cors");
+// Op = Sequelize.Op;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-const sequelize = new Sequelize('bng3ula9hx5zcetcsi4d',
-                                 'uefs9lkbgfgemj0f', 
-                                 'toIlg4BHZEg1ntEqvro7', {
-    host:"bng3ula9hx5zcetcsi4d-mysql.services.clever-cloud.com",
-    dialect: 'mysql'
-},{
-    timestamps: false
-})
-
-const db = require('./models');
-// const { GoogleSpreadsheet } = require('google-spreadsheet');
-
 const PORT = process.env.PORT || 8080;
+const workbook = XLSX.readFile("Info.xlsx");
+app.get("/", (req, res) => {
+  const categories = workbook.SheetNames;
+  res.json(categories);
+});
 
+app.get("/:catName", (req, res) => {
+  const catName = req.params.catName;
+  const makes = [];
+  let uniqueMakes = [];
+  //   for (const cat of workbook.SheetNames) {
+  for (const mk in workbook.Sheets[catName]) {
+    if (mk.startsWith("A")) {
+      makes.push(workbook.Sheets[catName][mk].v);
+    }
+    //   String(workbook.Sheets[catName][mk].v).trim().toLowerCase() ===
+    //     catName.toLowerCase()
+    // ) {
+    //   makes.push(
+    //     workbook.Sheets[catName]["A" + mk.substring(1, 3)].v
+    //       .trim()
+    //       .split("/")
+    //       .join(",")
+    //       .split(",")[0]
+    //       .toUpperCase()
+    //   );
+    // }
+    // }
+  }
+  uniqueMakes = [...new Set(makes)];
+  uniqueMakes.shift();
+  res.json(uniqueMakes);
+});
+app.get("/:catName/:makeName", (req, res) => {
+  const { makeName, catName } = req.params;
+  const models = [];
+  let uniqueModels = [];
+  const worksheet = workbook.Sheets[catName];
+  for (const mk in workbook.Sheets[catName]) {
+    if (
+      worksheet[mk].v &&
+      String(worksheet[mk].v).trim().toUpperCase() === makeName.toUpperCase()
+    ) {
+      models.push(
+        worksheet["B" + mk.substring(1, 3)].v
+          .trim()
+          .toUpperCase()
+          .split(" ")
+          .join(",")
+          .split("/")
+          .join(",")
+          .split(",")[0]
+      );
+    }
+  }
 
+  uniqueModels = [...new Set(models)];
+  res.json(uniqueModels);
+});
 
-app.get('/', (req,res)=>{
-    db.Make.findAll()
-    .then(makes => res.json(makes))
-    .catch(err=>res.status(500).json(err));
-})
+app.get("/:catName/:makeName/:modelName", (req, res) => {
+  console.log(req.params);
+  const { catName, makeName, modelName } = req.params;
+  const products = [];
+  let uniqueProducts = [];
+  const worksheet = workbook.Sheets[catName];
+  for (const mk in workbook.Sheets[catName]) {
+    if (
+      worksheet[mk].v &&
+      String(worksheet[mk].v).trim().toUpperCase() === makeName.toUpperCase()
+    ) {
+      if (
+        worksheet["B" + mk.substring(1, 3)].v &&
+        String(worksheet["B" + mk.substring(1, 3)].v)
+          .trim()
+          .toUpperCase() === modelName.toUpperCase()
+      ) {
+        products.push({
+          year: worksheet["C" + mk.substring(1, 3)].v
+            .trim()
+            .toUpperCase()
+            .split(" ")
+            .join(",")
+            .split("/")
+            .join(",")
+            .split(",")[0],
+          price: (worksheet["H" + mk.substring(1, 3)]
+             && worksheet["H1"].v === 'price') ? worksheet["H" + mk.substring(1, 3)].v 
+                .trim()
+                .toUpperCase()
+                .split(" ")
+                .join(",")
+                .split("/")
+                .join(",")
+                .split(",")[0]
+            : null,
+          img: worksheet["G" + mk.substring(1, 3)]
+            ? worksheet["G" + mk.substring(1, 3)].v
+                .trim()
+                .toUpperCase()
+                .split(" ")
+                .join(",")
+                .split("/")
+                .join(",")
+                .split(",")[0]
+            : null,
+          qfpp: worksheet["F" + mk.substring(1, 3)]
+            ? worksheet["F" + mk.substring(1, 3)].v
+                .trim()
+                .toUpperCase()
+                .split(" ")
+                .join(",")
+                .split("/")
+                .join(",")
+                .split(",")[0]
+            : null,
+        });
+      }
+    }
+  }
+  uniqueProducts = [...new Set(products)];
+  res.json(uniqueProducts);
+});
 
-
-app.get('/products/:qfpp',(req, res)=>{
-    const qfpp = req.params.qfpp;
-
-    db.parts.find({
-        where:{
-            qfpp: qfpp
-        }
-    })
-
-    .then(parts=>res.json(parts))
-    .catch(err=>res.status(500).json(err));
-})
-app.get('/:makeName', (req,res)=>{
-    const makeName = req.params.makeName;
-
-    db.Make.find({
-        where:{
-            name: makeName
-        }
-    })
-    .then(makes=>
-        
-        db.Model.findAll({
-        where:{
-            make_id: makes.id
-        }
-    }))
-    .then(models =>{ 
-        res.json(models.map(model =>model.name))
-    })
-    .catch(err => res.status(500).json(err))
-})
-app.get('/:makeName/:modelName', (req,res)=>{
-
-    const modelName = req.params.modelName;
-    db.Model.findAll({
-        where: {
-            name: modelName
-        },
-        include: [db.Category]
-    })
-    .then(models =>        
-        res.json(models[0].Categories.map(cat => cat.name)))
-    .catch(err=> res.status(500).json(err))
-})
-
-
-
-app.get('/:makeName/:modelName/:catName',(res,req)=>{
-
-    const catName = req.req.params.catName;
-    const modelName = req.req.params.modelName;
-
-    db.Model.find({
-        where: {
-            name: modelName,
-        },
-        include:[
-            {
-                model:db.Cateogry,
-                where: {
-                    name: catName
-                }
-            }
-        ]
-        
-    })
-    .then(parts => db.modelcat.findAll({
-        where:{
-            cat_id: category.id,
-        }
-    }))
-    .then(categories => res.res.json(categories))
-    .catch(err=> res.res.status(500).json(err))
-})
-
-
-app.listen(PORT, ()=>{
-    console.log(`server running on ${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
