64162d92f710fef2d39ce8a900f07c591ec1bfe3
Pouyan <pouyan.zarbafian@hotmail.com>
2024-02-03 20:18:31 -0500

    update server link

--- frontend/src/Components/ProductList.js
+++ frontend/src/Components/ProductList.js
@@ -30,7 +30,7 @@ export default function ProductList(props) {
       .then((res) => res.json())
       .then((data) => setProducts(data));
     // .catch(err => res.status(500).json(err))
-  }, [props.catName]);
+  }, [props.catName, props.modelName, props.makeName]);
 
   return (
     <div className="header__navbar--level2">
